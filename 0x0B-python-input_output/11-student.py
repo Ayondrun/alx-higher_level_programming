87bdcaa4356f8e0c12ef9e8ed7548ba4604ed1f7
Ayondrun <ayobamijoelobadare@gmail.com>
2022-10-25 23:08:24 +0000

    hot fix

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,34 +1,27 @@
-i#!/usr/bin/python3
-"""
-Contains the clas "Student"
-"""
-
-
+#!/usr/bin/python3
 class Student:
-    """Representation of a student"""
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
-        if attrs is None:
-            return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
+        class_d = self.__dict__
+        sel_d = dict()
+
+        if type(attrs) is list:
+            for attr in attrs:
+                if type(attr) is not str:
+                    return class_d
+
+                if attr in class_d:
+                    sel_d[attr] = class_d[attr]
+
+            return sel_d
+
+        return class_d
 
     def reload_from_json(self, json):
-        """replaces all attributes of the Student instance"""
-        for key in json:
-            try:
-                setattr(self, key, json[key])
-            except:
-                pass
+        for i in json:
+            if i in self.__dict__.keys():
+                self.__dict__[i] = json[i]
